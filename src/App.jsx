be7d25ef0b9be488a4b86508b04832bec554d3bc
mubeen-barcha <mubeentanha235@gmail.com>
2022-08-04 04:07:16 +0500

    complete the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import Intro from "./components/intro/Intro";
 import About from "./components/about/About";
 import Productlist from "./components/productList/Productlist";
 import Contact from "./components/contact/Contact";
+import Toggle from "./components/toggle/Toggle";
+import { ThemeContext } from "./context";
 
 
 
@@ -10,10 +12,13 @@ import Contact from "./components/contact/Contact";
 
 
 function App() {
+
+  const theme = useContext(ThemeContext)
+  const darkMode = theme.state.darkMode;
   return (
     <>
-      <div>
-       
+      <div style={{backgroundColor: darkMode ?"#222" : "white", color: darkMode && "white"}}>
+        <Toggle/>
         <Intro/>
         <About/>
         <Productlist/>
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,79 @@
-import React from 'react'
-import "./contact.css"
+import React, { useContext, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import "./contact.css";
+import Phone from "../../img/phone.png";
+import Email from "../../img/email.png";
+import Address from "../../img/address.png";
+import { ThemeContext } from "../../context";
 
 function Contact() {
+  const formRef = useRef();
+
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
+
+  const [done, setDone] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_sqe2gfv",
+        "template_qbm7vxa",
+        formRef.current,
+        "dvnojY9igzYYpD_Kc"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true)
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  console.log(handleSubmit)
+
   return (
-    <div className='contact'>Contact</div>
-  )
+    <div className="c">
+      <div className="c-bg"></div>
+      <div className="c-wrapper">
+        <div className="c-left">
+          <h1 className="c-title">Let's discuss your project</h1>
+          <div className="c-info">
+            <div className="c-info-item">
+              <img src={Phone} alt="" className="c-icon" />
+              +9232743958
+            </div>
+            <div className="c-info-item">
+              <img src={Email} alt="" className="c-icon" />
+              contact@email.com
+            </div>
+            <div className="c-info-item">
+              <img src={Address} alt="" className="c-icon" />
+              245 king street, touterie victoria 8530 australia
+            </div>
+          </div>
+        </div>
+        <div className="c-right">
+          <p className="c-desc">
+            <b>what is your story?</b> Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Pariatur quod hic earum quidem praesentium vitae.
+          </p>
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Name" name="user_name" />
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Subject" name="user_subject" />
+            <input style={{backgroundColor: darkMode && "#333"}} type="text" placeholder="Email" name="user_email" />
+            <textarea style={{backgroundColor: darkMode && "#333"}} rows="5" placeholder="Message" name="message"></textarea>
+            <button>Submit</button>
+            {done && "Thank you...."}
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
